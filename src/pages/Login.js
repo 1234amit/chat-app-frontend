304cab1cc5594a43c1c2434d733bca351f54e277
@1234amit <cseiubatgoswami@gmail.com>
2022-07-17 02:02:13 +0600

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import Logo from "../assets/logo.png"
+
+const Login = () => {
+
+    const handleChange = () =>{
+        console.log("found target value")
+    }
+
+    const handleSubmit = () =>{
+        console.log("button clicked")
+    }
+
+
+    return (
+
+        <div>
+            <FormContainer>
+                <form action="" onSubmit={(event) => handleSubmit(event)}>
+                    <div className="brand">
+                        <img src={Logo} alt="logo" />
+                        <h1>happy</h1>
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        name="username"
+                        onChange={(e) => handleChange(e)}
+                        min="3"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={(e) => handleChange(e)}
+                    />
+                    <button type="submit">Log In</button>
+                    <span>
+                        Don't have an account ? <Link to="/register">Create One.</Link>
+                    </span>
+                </form>
+            </FormContainer>
+        </div>
+    )
+    }
+
+
+const FormContainer = styled.div`
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 1rem;
+        align-items: center;
+        background-color: #131324;
+
+        .brand{
+            display: flex;
+            align-items: center;
+            gap: 1rem;
+            justify-content: center;
+
+            img {
+            height: 5rem;
+            border-radius: 10px;
+            }
+
+            h1 {
+                color: white;
+                text-transform: uppercase;
+            }
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+            background-color: #00000076;
+            border-radius: 2rem;
+            padding: 5rem;
+          }
+
+          input {
+            background-color: transparent;
+            padding: 1rem;
+            border: 0.1rem solid #4e0eff;
+            border-radius: 0.4rem;
+            color: white;
+            width: 100%;
+            font-size: 1rem;
+            &:focus {
+              border: 0.1rem solid #997af0;
+              outline: none;
+            }
+          }
+
+
+          button {
+            background-color: #4e0eff;
+            color: white;
+            padding: 1rem 2rem;
+            border: none;
+            font-weight: bold;
+            cursor: pointer;
+            border-radius: 0.4rem;
+            font-size: 1rem;
+            text-transform: uppercase;
+            &:hover {
+              background-color: #4e0eff;
+            }
+          }
+
+          span {
+            color: white;
+            text-transform: uppercase;
+            a {
+              color: #4e0eff;
+              text-decoration: none;
+              font-weight: bold;
+            }
+          }
+
+
+`
+
+export default Login
